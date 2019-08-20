e0f601c4cb767813a7316837bab8893c7a01d126
Jefferson111 <e0309385@u.nus.edu>
2019-08-20 20:49:57 +0800

    Implemented Level-2 features; Add and List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The Duke program is a simple Personal Assistant Chatbot
  * that helps a person to keep track of various things.
@@ -9,9 +11,21 @@ public class Duke {
      * Entry point.
      */
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
         Ui ui = new Ui();
         ui.showWelcome();
-        while (!ui.readCommand().equals("bye")) {
+        while (true) {
+            String command = ui.readCommand();
+            if (!command.equals("bye") && !command.equals("list")) {
+                list.add(command);
+                System.out.println("added: " + command);
+            } else if (command.equals("bye")) {
+                break;
+            } else {
+                for (int i = 0; i < list.size(); ++i) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            }
         }
         ui.showBye();
     }
